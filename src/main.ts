e9d07753ae0e012ce6f430e65d61f29a2ebe0466
Wendy Katherine Gomez <wendykatherinnegomezv@gmail.com>
2026-03-31 12:23:44 -0500

    chore: initialize Vue group booking lab

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import "@wendy/retro-tech-foundation/css/core.css";
+import "@wendy/retro-tech-foundation/css/web.css";
+import "@wendy/retro-tech-foundation/css/recipes.css";
+import "@wendy/retro-tech-foundation/css/themes/phosphor.css";
+
+import "./styles/app.css";
+
+createApp(App).mount('#app')
